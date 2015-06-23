97c9614ae0ac23e9784bf68524f8113e16e2edc4
ironholds <ironholds@gmail.com>
2015-06-23 12:02:38 -0400

    add license, setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='floccus',
+    version="0.0.1",
+    author='Oliver Keyes',
+    author_email='ironholds@gmail.com',
+    url='https://github.com/Ironholds/floccus',
+    license=open('LICENSE').read(),
+    description='A set of utilities for handling CirrusSearch log data',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Text Processing :: General",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering"
+    ],
+)
