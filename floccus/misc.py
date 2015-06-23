cc8d5238153b650f6d8f0e1895dfd81802f0c0c6
ironholds <ironholds@gmail.com>
2015-06-23 12:45:13 -0400

    add misc

---
+++ floccus/misc.py
@@ -0,0 +1,11 @@
+import datetime
+
+"""
+Construct the file path to a particular day's cirrus logs on stat1002. Uses yesterday if no date is specified.
+"""
+def get_filepath(date = None):
+  if date is None:
+    date = datetime.date.fromordinal(datetime.date.today().toordinal()-1)
+  parsed_date = date.strftime("%Y%m%d")
+  path = "/a/mw-log/archive/CirrusSearchRequests.log-" + parsed_date + ".gz"
+  return path, date
