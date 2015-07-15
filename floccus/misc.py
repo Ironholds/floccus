a4c59a5ba727206f764995b9107ae43176264149
ironholds <ironholds@gmail.com>
2015-07-15 13:15:35 -0400

    add a writer

--- floccus/misc.py
+++ floccus/misc.py
@@ -1,4 +1,7 @@
 import datetime
+import os.path
+import csv
+from collections import Counter
 
 """
 Construct the file path to a particular day's cirrus logs on stat1002. Uses yesterday if no date is specified.
@@ -9,3 +12,18 @@ def get_filepath(date = None):
   parsed_date = date.strftime("%Y%m%d")
   path = "/a/mw-log/archive/CirrusSearchRequests.log-" + parsed_date + ".gz"
   return path
+
+
+"""
+Write out a counter
+"""
+
+def write_counter(counter, date, file):
+  if(os.path.exists(file) == False):
+    with open(file, "wb") as tsv_file:
+      write_obj = csv.writer(tsv_file, delimiter = "\t")
+      write_obj.writerow(["date","variable","value"])
+  with open(file, "ab") as tsv_file:
+    write_obj = csv.writer(tsv_file, delimiter = "\t")
+    for entry in counter:
+      write_obj.writerow([str(date), entry, counter[entry]])
